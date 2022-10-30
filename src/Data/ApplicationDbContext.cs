8a98f192fc4b1079aeda85c685d1d1dd1af5d36c
Gabrielle Alcantara <gabrielle.alcantara@sga.pucminas.br>
2022-10-30 05:58:30 -0300

    add user registration, login, basic shared layout and initial home page

---
+++ src/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using team_shirt.Models;
+
+namespace team_shirt.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
